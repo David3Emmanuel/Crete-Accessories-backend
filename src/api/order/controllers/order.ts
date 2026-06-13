16f08d37691a2c5b9c07d85a0a61295bcb61c83a
David3Emmanuel <david3emmanuel@gmail.com>
2026-06-13 17:29:59 +0100

    Secure order controller to filter by authenticated user

--- src/api/order/controllers/order.ts
+++ src/api/order/controllers/order.ts
@@ -1,3 +1,38 @@
 import { factories } from "@strapi/strapi";
 
-export default factories.createCoreController("api::order.order");
+export default factories.createCoreController(
+  "api::order.order",
+  ({ strapi }) => ({
+    async find(ctx) {
+      const user = ctx.state.user;
+      if (!user) {
+        return ctx.unauthorized("You must be logged in to view orders.");
+      }
+
+      ctx.query.filters = {
+        ...(ctx.query.filters as any),
+        user: { id: user.id },
+      };
+
+      return await super.find(ctx);
+    },
+
+    async findOne(ctx) {
+      const user = ctx.state.user;
+      if (!user) {
+        return ctx.unauthorized("You must be logged in to view orders.");
+      }
+
+      const { id } = ctx.params;
+      const order = await strapi.entityService.findOne("api::order.order", id, {
+        populate: ["user"],
+      });
+
+      if (!order || (order.user && (order.user as any).id !== user.id)) {
+        return ctx.notFound();
+      }
+
+      return await super.findOne(ctx);
+    },
+  })
+);
